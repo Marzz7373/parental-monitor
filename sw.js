75e174a0d2506da539d14a2b80ac919c28b6e819
Marzz7373 <mnmarzuqi@gmail.com>
2025-10-07 14:58:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,35 @@
-self.addEventListener('install', e => {
-  console.log('[Service Worker] Installed');
+// sw.js
+const CACHE_NAME = 'monitor-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  // Add your icon image files here once you create them
+  // 'icon-192.png',
+  // 'icon-512.png'
+];
+
+// Install event: cache all necessary assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// Fetch event: serve assets from cache first
 self.addEventListener('fetch', event => {
-  // Optionally cache or modify requests here
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
 });
